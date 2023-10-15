2f5fc7b20346e81ef5c2cdc3cc5538cf8cad97dd
Berkay Anduv <sygrok@hotmail.com>
2023-10-15 13:37:43 +0300

    CreatingStore

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,17 @@
-import classes from './Counter.module.css';
+import { useState } from "react";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const [count, setCount] = useState(0);
+
+  const toggleCounterHandler = () => {
+    setCount(count + 1);
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- {count} --</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
